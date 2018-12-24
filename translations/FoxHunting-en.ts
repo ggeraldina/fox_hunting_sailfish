f21f518a9531c939ed133702eb03cfb997abc481
Geraldina <grushevskaya.d.v@yandex.ru>
2018-12-24 22:21:35 +0300

    Added new files

---
+++ translations/FoxHunting-en.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GamePage</name>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="27"/>
+        <source>Game with computer</source>
+        <translation>Game with computer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="40"/>
+        <source>Computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="101"/>
+        <source>You</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoseGamePage</name>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="11"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="26"/>
+        <source>You lost.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="33"/>
+        <source>Better luck next time!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="40"/>
+        <source>New game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="46"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="12"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="27"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="34"/>
+        <source>New game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="40"/>
+        <source>Rules game</source>
+        <translation>Rules of the game</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="46"/>
+        <source>Select language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RulesPage</name>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="28"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="33"/>
+        <source>The rules</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="42"/>
+        <source>Rules of the game &apos;Fox Hunting&apos;</source>
+        <translation>Rules of the game &apos;Fox Hunting&apos;:
+
+Order of play:
+On the fields of the player and
+computer (field sizes: 9x9) randomly
+at four foxes hidden on each field.
+The player and the computer take
+turns making their shots each on
+his field, trying to find all the
+foxes before the opponent.
+The player shoots first.
+The winner is the one who
+first finds all the foxes.
+
+Feature of the game:
+1) After each well-aimed
+shot on a cell the fox displayed,
+and shot can be repeated.
+2) In case of a miss, a number is
+displayed, which shows the number
+of foxes hidden in all cells,
+located on the same vertical,
+horizontal and on both diagonals
+(both undiscovered and found foxes
+are taken into account). The next
+move is given to the opponent.
+3) Foxes can not be located
+in the same cell.
+4) Foxes can&apos;t be located in
+neighboring cells.
+</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="47"/>
+        <source>New game</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SelectLanguagePage</name>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="12"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="27"/>
+        <source>Select language, please</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="34"/>
+        <source>Выберите, пожалуйста, язык</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TablesCompUser</name>
+    <message>
+        <location filename="../src/TablesCompUser.h" line="50"/>
+        <source>fox</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WinGamePage</name>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="11"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="26"/>
+        <source>You win!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="33"/>
+        <source>Congratulate!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="40"/>
+        <source>New game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="46"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/FoxHunting-ru.ts
@@ -0,0 +1,198 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="8"/>
+        <source>Fox hunting</source>
+        <translation type="unfinished">Охота на лис</translation>
+    </message>
+</context>
+<context>
+    <name>GamePage</name>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="27"/>
+        <source>Game with computer</source>
+        <translation>Игра против компьютера</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="40"/>
+        <source>Computer</source>
+        <translation>Компьютер</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/GamePage.qml" line="101"/>
+        <source>You</source>
+        <translation>Вы</translation>
+    </message>
+</context>
+<context>
+    <name>LoseGamePage</name>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="11"/>
+        <source>Fox hunting</source>
+        <translation>Охота на лис</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="26"/>
+        <source>You lost.</source>
+        <translation>Вы проиграли</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="33"/>
+        <source>Better luck next time!</source>
+        <translation>В следующий раз повезет!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="40"/>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/LoseGamePage.qml" line="46"/>
+        <source>Menu</source>
+        <translation>Меню</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="12"/>
+        <source>Fox hunting</source>
+        <translation>Охота на лис</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="27"/>
+        <source>Menu</source>
+        <translation>Меню</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="40"/>
+        <source>Rules game</source>
+        <translation>Правила игры</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="34"/>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MenuPage.qml" line="46"/>
+        <source>Select language</source>
+        <translation>Выбрать язык</translation>
+    </message>
+</context>
+<context>
+    <name>RulesPage</name>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="28"/>
+        <source>Fox hunting</source>
+        <translation>Охота на лис</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="33"/>
+        <source>The rules</source>
+        <translation>Правила</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="42"/>
+        <source>Rules of the game &apos;Fox Hunting&apos;</source>
+        <translation>Правила игры &apos;Охота на лис&apos;:
+
+Порядок игры:
+На полях игрока и компьютера
+(размеры полей: 9х9) случайным
+образом спрятано по четыре лисы.
+Игрок и компьютер по очереди
+делают свои выстрелы каждый на
+своем поле, пытаясь отыскать
+всех лис раньше противника.
+Игрок стреляет первым.
+Победителем становится тот,
+кто первым найдет всех лис,
+загаданных противником.
+
+Особенности игры:
+1) После каждого меткого
+выстрела по клетке открывается
+лиса и предоставляется
+повторный выстрел.
+2) В случае промаха открывается
+число, которое показывает
+количество лис, спрятанных
+во всех клетках,
+находящихся на одной вертикали,
+горизонтали и по обеим
+диагоналям (Учитываются как
+ненайденные, так и найденные
+лисы).
+Следующий ход предоставляется
+противнику.
+3) Несколько лис не могут
+находится в одной клетке.
+4) Лисы не могут располагаться
+в соседних клетках.
+</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RulesPage.qml" line="47"/>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+</context>
+<context>
+    <name>SelectLanguagePage</name>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="12"/>
+        <source>Fox hunting</source>
+        <translation>Охота на лис</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="27"/>
+        <source>Select language, please</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SelectLanguagePage.qml" line="34"/>
+        <source>Выберите, пожалуйста, язык</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TablesCompUser</name>
+    <message>
+        <location filename="../src/TablesCompUser.h" line="50"/>
+        <source>fox</source>
+        <translation>лис</translation>
+    </message>
+</context>
+<context>
+    <name>WinGamePage</name>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="11"/>
+        <source>Fox hunting</source>
+        <translation>Охота на лис</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="26"/>
+        <source>You win!</source>
+        <translation>Вы победили!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="33"/>
+        <source>Congratulate!</source>
+        <translation>Поздравляем!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="40"/>
+        <source>New game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WinGamePage.qml" line="46"/>
+        <source>Menu</source>
+        <translation>Меню</translation>
+    </message>
+</context>
+</TS>
